9907cbd46c41b2ab743ce0dc2cbbe5299a0156af
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-10-15 16:23:36 +0200

    Reworked event cards

--- src/components/Events/EventCard.tsx
+++ src/components/Events/EventCard.tsx
@@ -30,11 +30,9 @@ function EventCard({ data, toggleForm }: EventCardType) {
       <div className="event-content">
         <div className="event-card-header">
           <h2 className="event-name big bold">{data.eventName}</h2>
-          <div className="event-data bold">
-            <p className="event-agenda">{data.agenda}</p>
-            <p className="event-time">{data.time}</p>
-          </div>
+          <p className="event-time">{data.time}</p>
         </div>
+        <p className="event-agenda">{data.agenda}</p>
         <div className="event-note">{data.note}</div>
       </div>
     </div>
